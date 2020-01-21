7727a43275a09102d514f473751085dcc6b42571
gongqiangwei <2401330862@qq.com>
2020-01-21 16:59:17 +0800

    初始化

--- ssm/src/main/java/com/gqw/staticdatasource/datasoure/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.gqw.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.gqw.demo.entity.UserEntity;
-
-public interface UserMapper extends BaseMapper<UserEntity> {
-
-}
